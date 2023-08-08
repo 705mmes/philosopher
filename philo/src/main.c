6159b262abf007850ee2f5b9c02239e3edae7c69
Samuel Munio <smunio@k0r4p7.42mulhouse.fr>
2023-08-08 15:20:34 +0200

    is_done

--- philo/src/main.c
+++ philo/src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sammeuss <sammeuss@student.42.fr>          +#+  +:+       +#+        */
+/*   By: smunio <smunio@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 10:50:32 by sammeuss          #+#    #+#             */
-/*   Updated: 2023/08/08 14:03:54 by sammeuss         ###   ########.fr       */
+/*   Updated: 2023/08/08 15:16:54 by smunio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	free_all(t_data *d)
 	i = 0;
 	pthread_mutex_destroy(d->ded_lock);
 	pthread_mutex_destroy(d->print_lock);
-	while (i < d->nb_philo)
+	while (i != d->nb_philo)
 	{
 		pthread_mutex_destroy(d->philo[i]->l_fork);
 		pthread_mutex_destroy(d->philo[i]->meal_lock);
@@ -36,21 +36,23 @@ void	free_all(t_data *d)
 int	main(int argc, char **argv)
 {	
 	t_data	data;
+	int		r;
 
+	r = 0;
 	if (argc != 5 && argc != 6)
 		return (printf("Error\nWrong amount of args\n"));
 	if (check_args(argv))
 		return (printf("Error\nWrong args\n"));
-	if (init_data(&data, argc, argv) == 2)
+	r = init_data(&data, argc, argv);
+	if (r == 2)
 		data.philo[0] = create_philo(0, &data);
-	else if (init_data(&data, argc, argv) == 1)
+	else if (r == 1)
 		return (printf("Error\nSomething's wrong\n"));
 	init_philo(&data);
 	if (data.error == 1)
 		return (1);
 	run(&data);
 	free_all(&data);
-	system("leaks philo");
 	return (0);
 }
  
